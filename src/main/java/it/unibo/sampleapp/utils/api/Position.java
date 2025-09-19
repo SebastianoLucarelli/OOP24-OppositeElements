75d75a8c0f43d31d0ba071f4a1a7588bf50261a5
Emily <emily.pedini@studio.unibo.it>
2025-09-19 16:18:40 +0200

    checkstyle arrangement

--- src/main/java/it/unibo/sampleapp/utils/api/Position.java
+++ src/main/java/it/unibo/sampleapp/utils/api/Position.java
@@ -1,11 +1,10 @@
 package it.unibo.sampleapp.utils.api;
 
 /**
- * Interface for the positions in the game
+ * Interface for the positions in the game.
  */
-
 public interface Position {
-    
+
     /**
      * @return the x of the current position
      */
@@ -17,17 +16,25 @@ public interface Position {
     int getY();
 
     /**
-     * Change and set the new coordinate x of the current position 
+     * Change and set the new coordinate x of the current position.
+     *
      * @param x is the new coordinate
      */
     void setX(int x);
 
     /**
-     * Change and set the new coordinate y of the current position 
+     * Change and set the new coordinate y of the current position.
+     *
      * @param y is the new coordinate
      */
     void setY(int y);
 
+    /**
+     * Compares this position with another object.
+     *
+     * @param position the object to compare
+     * @return true if equal, false otherwise
+     */
+    @Override
     boolean equals(Object position);
-
 }
