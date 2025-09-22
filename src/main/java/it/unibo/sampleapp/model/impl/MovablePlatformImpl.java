17548876135a85e805fd0daf09699c55d62150cf
Sebastiano Lucarelli <sebastiano.lucarelli@studio.unibo.it>
2025-09-22 18:42:59 +0200

    creation of the Movable Platform Implementation

---
+++ src/main/java/it/unibo/sampleapp/model/impl/MovablePlatformImpl.java
@@ -0,0 +1,48 @@
+package it.unibo.sampleapp.model.impl;
+
+import it.unibo.sampleapp.model.api.MovableIPlatform;
+import it.unibo.sampleapp.utils.api.Position;
+
+public class MovablePlatformImpl implements MovableIPlatform {
+
+    public MovablePlatformImpl(){
+        
+    }
+
+    @Override
+    public Position getPosition() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+    }
+
+    @Override
+    public int getWidth() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getWidth'");
+    }
+
+    @Override
+    public int getHeight() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHeight'");
+    }
+
+    @Override
+    public void setPosition(Position position) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPosition'");
+    }
+
+    @Override
+    public void move() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'move'");
+    }
+
+    @Override
+    public int getSpeed() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSpeed'");
+    }
+    
+}
