30ab4af39aea1482d8fbcef72397b4897f201148
Sebastiano Lucarelli <sebastiano.lucarelli@studio.unibo.it>
2025-09-23 12:53:57 +0200

    create MovablePlatformImpl

--- src/main/java/it/unibo/sampleapp/model/impl/MovablePlatformImpl.java
+++ src/main/java/it/unibo/sampleapp/model/impl/MovablePlatformImpl.java
@@ -2,47 +2,89 @@ package it.unibo.sampleapp.model.impl;
 
 import it.unibo.sampleapp.model.api.MovableIPlatform;
 import it.unibo.sampleapp.utils.api.Position;
+import it.unibo.sampleapp.utils.impl.PositionImpl;
 
+/**
+ * Implementation of the MovableIPlatoform interface.
+ */
 public class MovablePlatformImpl implements MovableIPlatform {
 
-    public MovablePlatformImpl(){
-        
+    private Position position;
+    private final int width;
+    private final int height;
+    private final int distance;
+    private final int maxX;
+    private final int minX;
+
+    /**
+     * Constructor of the MovablePlatformImpl.
+     *
+     * @param position is the position of the platform
+     * @param width is the width of the platform
+     * @param height is the height of the platform
+     * @param distance is de distance that the platform move each time
+     * @param minX is the minimum X value that the platform can reach
+     * @param maxX is the maximum X value that the platform can reach
+     */
+    public MovablePlatformImpl(final Position position, final int width, final int height, final int distance, 
+    final int minX, final int maxX) {
+        this.position = new PositionImpl(position.getX(), position.getY());
+        this.width = width;
+        this.height = height;
+        this.distance = distance; 
+        this.minX = minX;
+        this.maxX = maxX;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Position getPosition() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+        return new PositionImpl(position.getX(), position.getY());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getWidth() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getWidth'");
+        return this.width;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getHeight() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getHeight'");
+        return this.height;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void setPosition(Position position) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setPosition'");
+    public void setPosition(final Position position) {
+        this.position = new PositionImpl(position.getX(), position.getY());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void move() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'move'");
+        final int newX = this.position.getX() + this.distance;
+        if (newX > this.minX && newX < this.maxX) {
+            position.setX(newX);
+        }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getSpeed() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getSpeed'");
+        return this.distance;
     }
-    
+
 }
