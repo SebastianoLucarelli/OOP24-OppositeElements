90690a67b8c7415e33106e69637dde56cc7c34c5
Benedetta Barone <benedetta.barone@studio.unibo.it>
2025-09-23 14:03:19 +0200

    Implementation of the classes regarding the players

---
+++ src/main/java/it/unibo/sampleapp/model/api/Player.java
@@ -0,0 +1,40 @@
+package it.unibo.sampleapp.model.api;
+
+import it.unibo.sampleapp.utils.api.Position;
+
+/**
+ * Interface that defines the two players.
+ */
+public interface Player {
+
+    /**
+     * Process controller commands.
+     *
+     * @param left if the player has move to left
+     * @param right if the player has move to right
+     * @param jump if the player has to jump
+     */
+    void input(boolean left, boolean right, boolean jump);
+
+    /**
+     * Update player logic.
+     *
+     * @param deltaTime time since last update
+     */
+    void updatePlayer(double deltaTime);
+
+    /**
+     * @return player's current position
+     */
+    Position getPosition();
+
+    /**
+     * @return player width
+     */
+    int getWidth();
+
+    /**
+     * @return player height
+     */
+    int getHeight();
+}
