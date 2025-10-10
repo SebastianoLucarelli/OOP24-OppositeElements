e8f7c9876d5ee3bcba168fee399d451b4903f5bf
Emily <emily.pedini@studio.unibo.it>
2025-10-10 16:00:09 +0200

    Creation of Collisions and CollisionFactory interfaces to model palyer-object interactions. Implementation of CollisionQueue to manage collision events

---
+++ src/main/java/it/unibo/sampleapp/model/collision/api/Collisions.java
@@ -0,0 +1,16 @@
+package it.unibo.sampleapp.model.collision.api;
+
+import it.unibo.sampleapp.model.game.api.Game;
+
+/**
+ * Represents a Collision event that can be handled in the game.
+ */
+public interface Collisions {
+
+    /**
+     * Applies the effect of the collision to the game.
+     *
+     * @param game the current game instance where the collision occurred
+     */
+    void manageCollision(Game game);
+}
